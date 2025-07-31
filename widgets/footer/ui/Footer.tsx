888fd0dff318c2f421e274560f1a9a9409dd4600
Vladimir434 <korolevskijvladimir73@gmail.com>
2025-07-31 12:03:25 +0600

    add footer

---
+++ widgets/footer/ui/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className='w-full bg-[#222] flex items-center justify-center h-[170px]'>
+      <h2 className="text-[44px] text-[#fff]">Footer</h2>
+    </div>
+  )
+}
+
+export default Footer
